a378a670dec58987cd825f66ccefedb2753d10ca
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-23 23:03:04 +0200

    ADD: model to save users to database

---
+++ twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/model/Profile.java
@@ -0,0 +1,51 @@
+package com.mohamed.halim.twitterclone.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder(toBuilder = true)
+@Entity
+public class Profile implements UserDetails {
+    @Id
+    private String username;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
