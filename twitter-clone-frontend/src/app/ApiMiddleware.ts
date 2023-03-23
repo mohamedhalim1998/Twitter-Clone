b7c3208e58059926acd897a462b47865e61084db
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-24 00:18:01 +0200

    ADD: setup api middleware

---
+++ twitter-clone-frontend/src/app/ApiMiddleware.ts
@@ -0,0 +1,65 @@
+import { createAction } from "@reduxjs/toolkit";
+import axios, { AxiosError, AxiosResponse, Method } from "axios";
+import Cookies from "js-cookie";
+import _ from "lodash";
+import { AnyAction, Middleware } from "redux";
+
+export interface ApiCallParams {
+  url: string;
+  method?: Method;
+  onSuccess?: AnyAction | string;
+  onError?: string;
+  body?: object;
+  headers?: object;
+  useJwtToken?: boolean;
+}
+
+export const apiCall = createAction<ApiCallParams>("apiCall");
+
+const apiMiddleware: Middleware =
+  (store) => (next) => async (action: AnyAction) => {
+    console.log(action);
+    if (action.type !== apiCall.type) {
+      console.log("next", next);
+
+      return next(action);
+    }
+    const payload = action.payload as ApiCallParams;
+    next(action);
+    const token = Cookies.get("access_token");
+    axios
+      .request({
+        url: payload.url,
+        method: payload.method ? payload.method : "GET",
+        data: payload.body,
+        headers: { ...payload.headers, Authorization: "Bearer " + token },
+      })
+      .then((response: AxiosResponse<any, any>) => {
+        if (payload.onSuccess != null) {
+          if (_.isString(payload.onSuccess))
+            store.dispatch({
+              type: payload.onSuccess,
+              payload: {
+                headers: response.headers,
+                data: response.data,
+                status: response.status,
+              },
+            });
+          else store.dispatch(payload.onSuccess as AnyAction);
+        }
+      })
+      .catch((error: AxiosError) => {
+        const response = error.response!!;
+        if (payload.onError != null) {
+          store.dispatch({
+            type: payload.onError,
+            payload: {
+              headers: response.headers,
+              data: response.data,
+              status: response.status,
+            },
+          });
+        }
+      });
+  };
+export default apiMiddleware;
--- twitter-clone-frontend/src/app/Store.ts
+++ twitter-clone-frontend/src/app/Store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import apiMiddleware from "./ApiMiddleware";
 
 export const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["apiCall"],
+      },
+    }).concat(apiMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
