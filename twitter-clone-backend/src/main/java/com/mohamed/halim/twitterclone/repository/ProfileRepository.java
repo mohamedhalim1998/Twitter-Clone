1daec96a6d5d21e296e15289ed4be8faa6852745
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-23 23:24:14 +0200

    EDIT: user optional instead of acutal object in repo

--- twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/repository/ProfileRepository.java
+++ twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/repository/ProfileRepository.java
@@ -1,9 +1,11 @@
 package com.mohamed.halim.twitterclone.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mohamed.halim.twitterclone.model.Profile;
 
 public interface ProfileRepository extends JpaRepository<Profile, String> {
-    Profile findByUsername(String username);
+    Optional<Profile> findByUsername(String username);
 }
--- twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/security/UserDetailsServiceImpl.java
+++ twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/security/UserDetailsServiceImpl.java
@@ -16,7 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return profileRepository.findByUsername(username);
+        return profileRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 
 }
