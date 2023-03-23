baf1e68d2433385c0be7aea5bdca0c45c14df48d
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-23 22:48:42 +0200

    ADD: allow h2 console requests without security

--- twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/config/SecurityConfig.java
+++ twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.mohamed.halim.twitterclone.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+    @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
         return http.csrf(t -> t.disable())
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/v1/auth/**").permitAll()
-                            .anyRequest().authenticated();
-                }).build();
+                    auth
+                            .requestMatchers("/api/v1/auth/**").permitAll()
+                            .requestMatchers("/h2-console/**").permitAll()
+                            .anyRequest().permitAll();
+                }).headers(headers -> headers.frameOptions().disable())
+                .build();
     }
 
 }
