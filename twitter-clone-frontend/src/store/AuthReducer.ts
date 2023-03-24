69009c4dfc235d78cfb26c14b2126fb3ab55a4e3
MohamedHalim <mohamedhalim1998@gmail.com>
2023-03-25 00:44:17 +0200

    ADD: auth reducer to hndle authentication actions

---
+++ twitter-clone-frontend/src/store/AuthReducer.ts
@@ -0,0 +1,32 @@
+import { createAction, createReducer, PayloadAction } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
+import { apiCall } from "./ApiMiddleware";
+
+export const signup = (username: string, password: string, email: string) =>
+  apiCall({
+    url: "http://localhost:8080/api/v1/auth/register",
+    onSuccess: saveJwtTokenFromResponse.toString(),
+    method: "POST",
+    body: {
+      username,
+      password,
+      email,
+    },
+  });
+
+export const saveJwtTokenFromResponse = createAction<any>(
+  "saveJwtTokenFromResponse"
+);
+export default createReducer(
+  {},
+  {
+    [saveJwtTokenFromResponse.type]: (
+      state: any,
+      action: PayloadAction<any>
+    ) => {
+      console.log(action.payload.headers);
+      const token = "access_token" as keyof typeof action.payload.headers;
+      Cookies.set("access_token", action.payload.headers[token]);
+    },
+  }
+);
