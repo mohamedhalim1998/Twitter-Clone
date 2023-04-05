f7302b9fd96daf3761dd05fbc8515245f5375585
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-05 02:32:01 +0200

    ADD: add some more test data

--- twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/config/DataInit.java
+++ twitter-clone-backend/src/main/java/com/mohamed/halim/twitterclone/config/DataInit.java
@@ -1,16 +1,30 @@
 package com.mohamed.halim.twitterclone.config;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.mohamed.halim.twitterclone.model.Attachment;
+import com.mohamed.halim.twitterclone.model.AttachmentType;
+import com.mohamed.halim.twitterclone.model.Media;
+import com.mohamed.halim.twitterclone.model.MediaDimension;
+import com.mohamed.halim.twitterclone.model.MediaType;
 import com.mohamed.halim.twitterclone.model.Profile;
 import com.mohamed.halim.twitterclone.model.Tweet;
+import com.mohamed.halim.twitterclone.model.TweetRefrence;
+import com.mohamed.halim.twitterclone.model.TweetRefrenceType;
+import com.mohamed.halim.twitterclone.repository.MediaRepository;
 import com.mohamed.halim.twitterclone.repository.ProfileRepository;
 import com.mohamed.halim.twitterclone.repository.TweetRepository;
+import com.mohamed.halim.twitterclone.service.MediaService;
 
 import lombok.AllArgsConstructor;
 
@@ -20,19 +34,52 @@ public class DataInit implements CommandLineRunner {
     private PasswordEncoder encoder;
     private ProfileRepository profileRepository;
     private TweetRepository tweetRepository;
+    private MediaRepository mediaRepository;
 
     @Override
     public void run(String... args) throws Exception {
         Profile profile = Profile.builder().username("user1")
+                .fullname("Mohamed Halim")
+                .profileImageUrl("http://localhost:8080/api/v1/media/default_profile.png")
+                .coverImageUrl("http://localhost:8080/api/v1/media/default_cover.jpg")
+                .bio("this my user bio Ullamco duis ex proident dolore magna tempor culpa.")
+                .email("a@a.com")
+                .createdAt(new Date())
+                .password(encoder.encode("123"))
+                .build();      
+                  Profile profile2 = Profile.builder().username("user2")
+                .fullname("You-Know-Who")
+                .profileImageUrl("http://localhost:8080/api/v1/media/default_profile.png")
+                .coverImageUrl("http://localhost:8080/api/v1/media/default_cover.jpg")
+                .bio("this my user bio Ullamco duis ex proident dolore magna tempor culpa.")
                 .email("a@a.com")
                 .createdAt(new Date())
                 .password(encoder.encode("123"))
                 .build();
+        Tweet tweet = Tweet.builder().text("this my pinned tweet").createdDate(LocalDateTime.now())
+                .authorId("user1").build();
+        tweet = tweetRepository.save(tweet);
+        profile.setPinnedTweetId(tweet.getId());
         profileRepository.save(profile);
+        profileRepository.save(profile2);
         for (int i = 0; i < 100; i++) {
             tweetRepository.save(Tweet.builder().text("Tweet from user1 id: " + i).createdDate(LocalDateTime.now())
                     .authorId("user1").build());
         }
+
+        Media media = mediaRepository.save(
+                Media.builder()
+                        .url("http://localhost:8080/api/v1/media/default_profile.png")
+                        .type(MediaType.IMAGE)
+                        .dimentions(new MediaDimension(1920, 1080))
+                        .build());
+        tweetRepository.save(Tweet.builder().text("Tweet from user1 with media").createdDate(LocalDateTime.now())
+                .attachment(new Attachment(AttachmentType.MEDIA, media.getId()))
+                .authorId("user1").build());
+        tweetRepository.save(Tweet.builder().text("Tweet from user1 with media and is retweet").createdDate(LocalDateTime.now())
+                .attachment(new Attachment(AttachmentType.MEDIA, media.getId()))
+                .tweetRefrence(new TweetRefrence(TweetRefrenceType.RETWEET, 1L))
+                .authorId("user2").build());
 
     }
 
