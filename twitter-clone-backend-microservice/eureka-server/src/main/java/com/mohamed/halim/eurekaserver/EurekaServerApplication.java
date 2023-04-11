4f935646c9ac04c202e917975b7aa48960438f24
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-11 02:30:17 +0200

    ADD: eureka server configuration

--- twitter-clone-backend-microservice/eureka-server/src/main/java/com/mohamed/halim/eurekaserver/EurekaServerApplication.java
+++ twitter-clone-backend-microservice/eureka-server/src/main/java/com/mohamed/halim/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.mohamed.halim.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
