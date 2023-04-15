a8b87cdb9fc8b3540f79c4379d0c4f4ca6a63ee1
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-16 00:50:36 +0200

    ADD: rabbit listener to handle verify queue

--- twitter-clone-backend-microservice/jwt-service/src/main/java/com/mohamed/halim/jwtservice/JwtService.java
+++ twitter-clone-backend-microservice/jwt-service/src/main/java/com/mohamed/halim/jwtservice/JwtService.java
@@ -4,12 +4,19 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
@@ -17,10 +24,11 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
-@Component
-@RequiredArgsConstructor
+@Service
+@AllArgsConstructor
 public class JwtService {
-
+  private final RabbitTemplate rabbit;
+  private Jackson2JsonMessageConverter converter;
   private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
   @RabbitListener(queues = "jwt.token.extract.username")
@@ -36,6 +44,19 @@ public class JwtService {
   @RabbitListener(queues = "jwt.token.generate")
   public String generateToken(@Payload User userDetails) {
     return generateToken(new HashMap<>(), userDetails);
+  }
+
+  @RabbitListener(queues = "jwt.token.verify")
+  public void verifyToken(Message message) {
+    String token = (String) converter.fromMessage(message);
+    var props = message.getMessageProperties();
+    User user = rabbit.convertSendAndReceiveAsType(
+        "profile", "profile.load.profile", extractUsername(token), new ParameterizedTypeReference<User>() {
+        });
+    if (user == null || !isTokenValid(token, user)) {
+      props.setHeader("error", "username or password is incorrect");
+    }
+    rabbit.send(props.getReplyTo(), converter.toMessage("", props));
   }
 
   public String generateToken(
