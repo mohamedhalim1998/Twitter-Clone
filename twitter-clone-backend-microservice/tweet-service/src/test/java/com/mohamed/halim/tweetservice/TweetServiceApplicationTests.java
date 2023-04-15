bbc5523aa949d3e9b0222743b7cbe1b8217553e2
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-16 01:07:43 +0200

    ADD: tweet service

---
+++ twitter-clone-backend-microservice/tweet-service/src/test/java/com/mohamed/halim/tweetservice/TweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.tweetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
