8853df683b4bd90912e8374c318170f4f03e8960
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-17 06:38:29 +0200

    ADD: gateway routes to profile service

---
+++ twitter-clone-backend-microservice/auth-service/src/main/java/com/mohamed/halim/authservice/config/GatewayConfig.java
@@ -0,0 +1,27 @@
+package com.mohamed.halim.authservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/users/**")
+                        .filters(f -> f.rewritePath("/api/v1/users/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://profile-service/"))
+                .build();
+    }
+
+    @LoadBalanced
+    @Bean
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+}
