72e49793c8c2cc72494400539e3a390474ea2746
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-17 07:34:36 +0200

    ADD: add error handler for error messages and prevent requeue faild message

--- twitter-clone-backend-microservice/jwt-service/src/main/java/com/mohamed/halim/jwtservice/config/RabbitMQConfig.java
+++ twitter-clone-backend-microservice/jwt-service/src/main/java/com/mohamed/halim/jwtservice/config/RabbitMQConfig.java
@@ -4,7 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper.TypePrecedence;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +15,10 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class RabbitMQConfig {
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
@@ -20,6 +26,20 @@ public class RabbitMQConfig {
         var converter = new Jackson2JsonMessageConverter(mapper);
         converter.setTypePrecedence(TypePrecedence.INFERRED);
         return converter;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setDefaultRequeueRejected(false);
+        factory.setErrorHandler((e) -> {
+            log.error(e.getMessage());
+        });
+        return factory;
     }
 
     @Bean
