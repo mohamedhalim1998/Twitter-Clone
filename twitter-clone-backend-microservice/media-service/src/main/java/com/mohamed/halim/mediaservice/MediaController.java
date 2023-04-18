d0742076e465c2573bd289f4192d0338603ae68e
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-19 00:12:51 +0200

    FIX: dtos package by adding it to local repo

--- twitter-clone-backend-microservice/media-service/src/main/java/com/mohamed/halim/mediaservice/MediaController.java
+++ twitter-clone-backend-microservice/media-service/src/main/java/com/mohamed/halim/mediaservice/MediaController.java
@@ -1,20 +1,27 @@
 package com.mohamed.halim.mediaservice;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
 
+import com.mohamed.halim.dtos.MediaDto;
 
 import lombok.AllArgsConstructor;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.tika.exception.TikaException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @RestController
-@RequestMapping("/api/v1/media")
+@RequestMapping("/media")
 @AllArgsConstructor
 public class MediaController {
     private MediaService mediaService;
@@ -22,6 +29,11 @@ public class MediaController {
     @GetMapping(value = "/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
     public Resource getMethodName(@PathVariable String name) throws FileNotFoundException {
         return mediaService.loadImage(name);
+    }
+
+    @PostMapping
+    public MediaDto saveMedia(@RequestPart("media") MultipartFile mediaFile) throws IllegalStateException, IOException, SAXException, TikaException {
+        return mediaService.saveMedia(mediaFile);
     }
 
 }
