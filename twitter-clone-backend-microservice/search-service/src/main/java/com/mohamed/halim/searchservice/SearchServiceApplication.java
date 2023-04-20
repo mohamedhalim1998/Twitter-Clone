214507e0a721d8fae0750ebf9ea15c8a074b3b4d
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-20 07:02:52 +0200

    ADD: serach service

---
+++ twitter-clone-backend-microservice/search-service/src/main/java/com/mohamed/halim/searchservice/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
---
+++ twitter-clone-backend-microservice/search-service/src/test/java/com/mohamed/halim/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
