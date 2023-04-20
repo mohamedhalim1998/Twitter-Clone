8b16602e798fe9bf12bbfda90c8edfee5cf24363
MohamedHalim <mohamedhalim1998@gmail.com>
2023-04-20 06:46:45 +0200

    ADD: no arg contructor for message dto

--- twitter-clone-backend-microservice/dtos/src/main/java/com/mohamed/halim/dtos/MessageDto.java
+++ twitter-clone-backend-microservice/dtos/src/main/java/com/mohamed/halim/dtos/MessageDto.java
@@ -2,11 +2,15 @@ package com.mohamed.halim.dtos;
 
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageDto {
     private Long id;
     private String from;
